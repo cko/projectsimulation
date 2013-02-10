437a63f751f97e7efec25689bce1d9adf0f5a866
Christine Koppelt <ch.ko123@googlemail.com>
2013-02-10 20:59:17 +0100

    initial commit

---
+++ kunde.cc
@@ -0,0 +1,73 @@
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+// 
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program.  If not, see http://www.gnu.org/licenses/.
+// 
+
+#include "kunde.h"
+
+Define_Module(Kunde);
+
+double probabilityRefuse;
+int anzahlFeatures;
+int timeStep = 3;
+
+void Kunde::initialize() {
+    cMessage *firstmsg = new cMessage("Feature");
+    firstmsg->addPar("creationTime");
+    firstmsg->par("creationTime").setDoubleValue(simTime().dbl());
+    send(firstmsg, "out");
+
+    cMessage *secondmsg = new cMessage("Feature");
+    secondmsg->addPar("creationTime");
+    secondmsg->par("creationTime").setDoubleValue((simTime() + timeStep).dbl());
+    scheduleAt(simTime() + timeStep, secondmsg);
+
+    probabilityRefuse = par("probRefuse").doubleValue();
+    anzahlFeatures = par("anzahlFeatures").longValue() - 2;
+
+}
+
+void Kunde::handleMessage(cMessage *msg) {
+    if (anzahlFeatures >= 0) {
+        //endSimulation();
+
+        if (msg->isSelfMessage()) {
+            //neue Nachrichten in Zeitschritten erzeugen
+            send(msg, "out");
+            //ev << "Neues Feature verschickt ";
+            cMessage *newmsg = new cMessage(
+                    "Feature " + (100 - anzahlFeatures));
+            newmsg->addPar("creationTime");
+            newmsg->par("creationTime").setDoubleValue(
+                    (simTime() + timeStep).dbl());
+            scheduleAt(simTime() + timeStep, newmsg);
+            anzahlFeatures -= 1;
+        } else {
+            //feature wurde bearbeitet
+            //soll Feature nochmal ueberarbeitet werden?
+            double randNumber = dblrand();
+            if (randNumber < probabilityRefuse) {
+                send(msg, "out");
+            } else {
+                //Anzahl verbleibender Features
+                //ev << "n = " << anzahlFeatures << "\n";
+                //komplette Bearbeitungszeit des features ausgeben
+                long totaltime = simTime().dbl()
+                        - msg->par("creationTime").doubleValue();
+                ev << totaltime << " ";
+                delete (msg);
+                recordScalar("totaltime", totaltime);
+            }
+        }
+    }
+}
---
+++ project.cc
@@ -0,0 +1,69 @@
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+// 
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program.  If not, see http://www.gnu.org/licenses/.
+// 
+
+#include "project.h"
+
+Define_Module(Project);
+
+    double probabilityError;
+    bool entwicklerHasFeature;
+    cQueue queue("featurequeue");
+
+void Project::initialize()
+{
+    probabilityError = par("probError").doubleValue();
+}
+
+void Project::handleMessage(cMessage *msg) {
+    double randNumber = dblrand();
+    //feature comes from entwickler and has no error
+    int inEntwicklerGate = strcmp("inEntwickler",
+            msg->getArrivalGate()->getName());
+
+    if (inEntwicklerGate == 0){
+        entwicklerHasFeature = false;
+    }
+
+
+
+
+    if (inEntwicklerGate == 0 && randNumber > probabilityError) {
+        send(msg, "outKunde");
+        if (!queue.isEmpty()){
+            cMessage *secondmsg = (cMessage *)queue.pop();
+            entwicklerHasFeature = true;
+            send(secondmsg, "outEntwickler");
+        }
+    } else {
+        queue.insert(msg);
+        if (!entwicklerHasFeature) {
+            msg = (cMessage *)queue.pop();
+            entwicklerHasFeature = true;
+            send(msg, "outEntwickler");
+        }
+    }
+
+    cDisplayString& connDispStr = getDisplayString();
+    if (queue.isEmpty()){
+        connDispStr.parse("p=163,174;i=block/cogwheel,#00FF00");
+    } else if (queue.getLength() < 10){
+        connDispStr.parse("p=163,174;i=block/cogwheel,#FFFF00");
+    } else {
+        connDispStr.parse("p=163,174;i=block/cogwheel,#FF0000");
+    }
+
+}
+
+
